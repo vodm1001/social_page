7b8e53a738091f14d7fc8be2968ac3c97d745c09
unknown <vodm1001@gmail.com>
2022-01-23 11:45:41 +0200

    version 0.1

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Header = () => {
+   return <header className='header'>
+      <img src='https://www.logodesign.net/images/illustration-logo.png'></img>
+   </header>
+}
+export default Header
